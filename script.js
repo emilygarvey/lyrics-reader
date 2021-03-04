fa74c1a04ea02c82a6b11024eed29d0544acc100
Michael <michael@Michaels-Air.lan>
2021-03-04 18:41:52 -0500

    Extension solution completed with various voice options

--- script.js
+++ script.js
@@ -1,80 +1,85 @@
-let apiBaseUrl = 'https://api.lyrics.ovh/v1/'
+let apiBaseUrl = "https://api.lyrics.ovh/v1/";
 
 let synth = window.speechSynthesis;
 let isMuted;
 let utterance;
 let volume = 0.5;
 let voices;
+let chosenVoice = 0;
+
 setTimeout(() => {
-    voices = synth.getVoices()
-    voices = filterVoices(voices);
-    console.log(voices);
-    appendVoicesToDropdown();
-}, 100);
+  voices = synth.getVoices();
+  voices = filterVoices(voices);
+  console.log(voices);
+  appendVoicesToDropdown();
+}, 500);
 
 function filterVoices(voices) {
-    return voices.filter((voice) => {
-        if (voice.lang === 'en-US') {
-            return true;
-        }
-        else {
-            return false;
-        }
-    })
+  return voices.filter((voice) => {
+    if (voice.lang === "en-US") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+}
+
+function updateVoice(index) {
+  chosenVoice = index;
 }
 
 function appendVoicesToDropdown() {
-    voices.forEach((voice, i) => {
-        $("#voice-select").append(`<option value=${i}>${voice.name}</option>`);
-    })
+  voices.forEach((voice, index) => {
+    $(".voice-buttons").append(
+      `<button onclick="updateVoice(${index})">${voice.name}</button>`
+    );
+  });
 }
 
+$("#get-lyrics").click(function () {
+  let artist = $("#artist").val();
+  let title = $("#title").val();
 
-$('#get-lyrics').click(function() {
-    let artist = $('#artist').val();
-    let title = $('#title').val();
+  getLyrics(artist, title).then(function (lyrics) {
+    $("#lyrics").text(lyrics);
 
-    getLyrics(artist, title).then(function(lyrics) {
-        $('#lyrics').text(lyrics);
-
-        speak(lyrics);
-    });
+    speak(lyrics);
+  });
 });
 
 function getLyrics(artist, title) {
-    $('#lyrics').text('Loading...');
+  $("#lyrics").text("Loading...");
 
-    return fetch(`${apiBaseUrl}${artist}/${title}`).then(function(response) {
-        return response.json().then(function(result) {
-            return result.lyrics;
-        });
+  return fetch(`${apiBaseUrl}${artist}/${title}`).then(function (response) {
+    return response.json().then(function (result) {
+      return result.lyrics;
     });
+  });
 }
 
-$('#pause-resume').click(toggleSpeech);
-
+$("#pause-resume").click(toggleSpeech);
 
 function speak(text) {
-    utterance = new SpeechSynthesisUtterance();
-    utterance.text = text;
-    utterance.volume = volume;
-    utterance.voice = voices[0];
-    synth.cancel();
-    isMuted = false;
-    synth.speak(utterance);
-    $('#pause-resume').text('PAUSE');
+  utterance = new SpeechSynthesisUtterance();
+  utterance.text = text;
+  utterance.volume = volume;
+  utterance.voice = voices[chosenVoice];
+  synth.cancel();
+  isMuted = false;
+  synth.speak(utterance);
+  $("#pause-resume").text("PAUSE");
 }
 
 function toggleSpeech() {
-    let button = $('#pause-resume');
+  let button = $("#pause-resume");
 
-    if (isMuted) {
-        synth.resume();
-        isMuted = false;
-        button.text('PAUSE');
-    } else {
-        synth.pause();
-        isMuted = true;
-        button.text('RESUME');
-    }
+  if (isMuted) {
+    synth.resume();
+    isMuted = false;
+    button.text("PAUSE");
+  } else {
+    synth.pause();
+    isMuted = true;
+    button.text("RESUME");
+  }
 }
