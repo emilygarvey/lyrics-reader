823217b43f787890b9f8a2d28c9224bb3ba9a507
Michael <michael@Michaels-Air.lan>
2021-03-04 19:04:02 -0500

    starter code with extension

--- script.js
+++ script.js
@@ -7,56 +7,6 @@ let volume = 0.5;
 let voices;
 let chosenVoice = 0;
 
-setTimeout(() => {
-  voices = synth.getVoices();
-  voices = filterVoices(voices);
-  console.log(voices);
-  addVoiceButtons();
-}, 500);
-
-function filterVoices(voices) {
-  return voices.filter((voice) => {
-    if (voice.lang === "en-US") {
-      return true;
-    } else {
-      return false;
-    }
-  });
-}
-
-function updateVoice(index) {
-  chosenVoice = index;
-}
-
-function addVoiceButtons() {
-  voices.forEach((voice, index) => {
-    $(".voice-buttons").append(
-      `<button onclick="updateVoice(${index})">${voice.name}</button>`
-    );
-  });
-}
-
-$("#get-lyrics").click(function () {
-  let artist = $("#artist").val();
-  let title = $("#title").val();
-
-  getLyrics(artist, title).then(function (lyrics) {
-    $("#lyrics").text(lyrics);
-
-    speak(lyrics);
-  });
-});
-
-function getLyrics(artist, title) {
-  $("#lyrics").text("Loading...");
-
-  return fetch(`${apiBaseUrl}${artist}/${title}`).then(function (response) {
-    return response.json().then(function (result) {
-      return result.lyrics;
-    });
-  });
-}
-
 $("#pause-resume").click(toggleSpeech);
 
 function speak(text) {
